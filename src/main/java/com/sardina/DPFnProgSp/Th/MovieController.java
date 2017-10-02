b4fb37358c430f13b897b0eed77a37a1c73a5361
Christopher Sardina <chrissardina@Chris-Sardina.local>
2017-10-02 19:10:02 -0400

    DP Fn Prog Spr&Th Movie API: worked w/api & wrote method to get movies.

--- src/main/java/com/sardina/DPFnProgSp/Th/MovieController.java
+++ src/main/java/com/sardina/DPFnProgSp/Th/MovieController.java
@@ -4,9 +4,17 @@ package com.sardina.DPFnProgSp.Th;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.RestTemplate;
 
-@Controller
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class MovieController {
+
+    static final String NOW_PLAYING = "https://api.themoviedb.org/3/movie/now_playing?api_key=be2a38521a7859c95e2d73c48786e4bb";
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String homeMovie() {
@@ -15,6 +23,8 @@ public class MovieController {
 
     @RequestMapping(path = "/now-playing", method = RequestMethod.GET)
     public String nowPlaying() {
+
+        //TODO: Pick up here
         return "now-playing";
     }
 
@@ -23,5 +33,13 @@ public class MovieController {
         return "medium-popular-long-name";
     }
 
+
+    public static List<Movie> getMovies(String route) {
+        List<Movie> movieList = new ArrayList<>();
+        RestTemplate restTemp = new RestTemplate();
+        restTemp.getForObject(route, Movie.class);
+        return movieList;
+
+    }
 
 }
